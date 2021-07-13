a0dae8ba99158a2daa0418a4689dbfcb950cb1d1
Daniel <danielchaimjacks@gmail.com>
2021-07-12 21:10:32 -0400

    Setup template pages

---
+++ client/src/pages/login/login_page.tsx
@@ -0,0 +1,6 @@
+import { observer } from 'mobx-react-lite'
+
+
+export const LoginPage = observer(() => {
+    return <>Login page</>
+})
--- client/src/router.tsx
+++ client/src/router.tsx
@@ -1,11 +1,16 @@
-import { observer } from 'mobx-react-lite';
-import { getUrlLocationPath } from './router_store';
+import { isEqual } from 'lodash'
+import { observer } from 'mobx-react-lite'
+import { HomePage } from './pages/home/home_page'
+import { LoginPage } from './pages/login/login_page'
+import { getUrlLocationPath } from './router_store'
 
 
 export const Router = observer(() => {
 
     const path = getUrlLocationPath()
 
-    return <>hi{path}</>
+    return isEqual(path, ['home']) ? <HomePage />
+        : isEqual(path, ['login']) ? < LoginPage />
+        : <> Page {path.join('/')} Not found </>
 })
 
