22efd8bf1eb9b573834fcbdd908bf70f7ac6df15
Josh Searle <joshryansearle@gmail.com>
2021-07-14 12:03:55 +1000

    Format server file

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,31 +1,31 @@
-import express, { Request, Response } from 'express';
-import { PrismaClient } from '@prisma/client';
+import express, { Request, Response } from 'express'
+import { PrismaClient } from '@prisma/client'
 
-const prisma = new PrismaClient();
-const app = express();
+const prisma = new PrismaClient()
+const app = express()
 
-const port = 5000;
+const port = 5000
 
-app.use(express.json());
+app.use(express.json())
 
 app.get('/health', (req, res) => {
-  res.status(200).json();
-});
+  res.status(200).json()
+})
 
 app.get('/users', async (req, res) => {
-  const users = await prisma.user.findMany();
-  res.status(200).json(users);
-});
+  const users = await prisma.user.findMany()
+  res.status(200).json(users)
+})
 
 app.post('/users', async (req, res) => {
-  const { username, password } = req.body;
+  const { username, password } = req.body
   try {
-    const result = await prisma.user.create({ data: { username, password } });
-    res.status(201).json(result);
+    const result = await prisma.user.create({ data: { username, password } })
+    res.status(201).json(result)
   } catch (e) {
-    res.status(400).json();
+    res.status(400).json()
   }
-});
+})
 
 app.post('/prisma/:table_name/:method', async (req, res) => {
   try {
@@ -34,15 +34,16 @@ app.post('/prisma/:table_name/:method', async (req, res) => {
     }
 
     // @ts-ignore
-    const result = await prisma[req.params.table_name][req.params.method](req.body)
+    const result = await prisma[req.params.table_name][req.params.method](
+      req.body
+    )
 
-    res.status(201).json(result);
+    res.status(201).json(result)
   } catch (error) {
-    res.status(400).send(error.toString());
+    res.status(400).send(error.toString())
   }
-
 })
 
 app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
-});
+  console.log(`Listening on port ${port}`)
+})
