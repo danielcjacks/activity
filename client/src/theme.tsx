8d2c77dfa333c572e61e06cb657bed5aafce3b57
Daniel <danielchaimjacks@gmail.com>
2021-07-17 00:58:59 -0400

    Add theme

---
+++ client/src/theme.tsx
@@ -0,0 +1,28 @@
+// override material ui theme properties here. 
+// Full example: https://material-ui.com/customization/default-theme/
+// Explanation: https://material-ui.com/customization/theming/#theming
+// default theme as json: https://gist.github.com/phusick/b5a114b9fd4baeca339b12160139635d
+
+import { createTheme } from '@material-ui/core'
+
+
+export const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#3f51b5'
+        },
+        secondary: {
+            main: '#f50057'
+        },
+        error: {
+            main: '#f44336'
+        },
+        warning: {
+            main: '#ff9800'
+        },
+        success: {
+            main: '#4caf50'
+        },
+        type: 'dark'
+    }
+})
