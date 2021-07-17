91e376f00af6394b724c32f0959aab680b22a019
Daniel <danielchaimjacks@gmail.com>
2021-07-17 00:57:54 -0400

    Add save button

---
+++ client/src/components/save_button.tsx
@@ -0,0 +1,34 @@
+import { Box, Fab, Tooltip } from '@material-ui/core'
+import { Save } from '@material-ui/icons'
+import { observer } from 'mobx-react-lite'
+import { theme } from '../theme'
+import { LoadingContainer } from './loading_container'
+
+
+type SaveButtonProps = {
+    can_save: boolean
+    is_loading: boolean
+    on_save: React.MouseEventHandler<HTMLButtonElement> | undefined
+}
+
+export const SaveButton = observer(({ can_save, is_loading, on_save}: SaveButtonProps) => {
+
+    return (
+        <Tooltip title={can_save ? 'Save changes' : 'No changes to save'}>
+            <Box position='fixed' bottom={15} right={15}>
+                <Fab
+                    color='primary'
+                    style={{ backgroundColor: theme.palette.success.main }}
+                    onClick={on_save}
+                    disabled={!can_save}
+                >
+                    <LoadingContainer
+                        is_loading={is_loading}
+                    >
+                        <Save fontSize='large' />
+                    </LoadingContainer>
+                </Fab>
+            </Box>
+        </Tooltip>
+    )
+})
