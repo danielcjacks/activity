025e7e9d2a08032ed98bf6f58f6f2e7596836a63
Daniel <danielchaimjacks@gmail.com>
2021-07-20 21:07:36 -0400

    Add save function

--- client/src/pages/values/value_store.ts
+++ client/src/pages/values/value_store.ts
@@ -1,4 +1,6 @@
 import { makeAutoObservable } from 'mobx'
+import { router_store } from '../../router_store'
+import { server_post } from '../../server_connector'
 
 
 class ValueStore {
@@ -9,7 +11,22 @@ class ValueStore {
         makeAutoObservable(this)
     }
 
-}
+    get value_id () {
+        return router_store.query.value_id
+    }
 
+    save_changes = () => {
+        const is_update = !!this.value_id
+        const prisma_method = is_update ? 'update' : 'create' // delete is done from the values table page
+        const prisma_body = {
+            data: {
+                ...this.value
+            }
+        }
+
+        server_post(`/prisma/value/${prisma_method}`, prisma_body)
+    }
+
+}
 
 export const value_store = (window as any).value_store = new ValueStore()
