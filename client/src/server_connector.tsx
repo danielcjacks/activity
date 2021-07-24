1ae2ba312a0084efcd0742162e137dc1b6b26304
Josh Searle <joshryansearle@gmail.com>
2021-07-24 17:59:43 +1000

    Fix server connector error handling

--- client/src/server_connector.tsx
+++ client/src/server_connector.tsx
@@ -4,27 +4,31 @@ import { shared_store } from './shared_store'
  * @param server_route a server route like '/login'
  */
 export const server_post = async (server_route, body) => {
-    try {
-        const response = await fetch(`http://localhost:5000${server_route}`, {
-            method: 'POST',
-            mode: 'cors',
-            cache: 'no-cache',
-            redirect: 'follow',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            referrerPolicy: 'no-referrer',
-            body: JSON.stringify(body)
-        })
-    
-        const json_response = await response.json()
-        if (json_response.error) {
-            shared_store.show_toast('error', <pre>Error: {json_response.error?.message}</pre>)
-        }
-        return json_response
-    } catch (error) {
-        console.log(error)
-        shared_store.show_toast('error', 'Cant connect to server')
-        return undefined
+  try {
+    const response = await fetch(`http://localhost:5000${server_route}`, {
+      method: 'POST',
+      mode: 'cors',
+      cache: 'no-cache',
+      redirect: 'follow',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      referrerPolicy: 'no-referrer',
+      body: JSON.stringify(body),
+    })
+
+    const json_response = await response.json()
+
+    if (!response.ok) {
+      shared_store.show_toast(
+        'error',
+        <pre>Error: {json_response?.message}</pre>
+      )
     }
+    return json_response
+  } catch (error) {
+    console.log(error)
+    shared_store.show_toast('error', 'Cant connect to server')
+    return undefined
+  }
 }
