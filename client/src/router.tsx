cdef5701a1bb9a8bf4b12aa9fd1010b6f02c941b
Josh Searle <joshryansearle@gmail.com>
2021-07-24 17:03:53 +1000

    Redirect to login if not auth

--- client/src/router.tsx
+++ client/src/router.tsx
@@ -5,11 +5,14 @@ import { LoginPage } from './pages/login/login_page'
 import { ValuesPage } from './pages/values/values_page'
 import { ValuePage } from './pages/values/value_page'
 import { get_url_location_path } from './router_store'
-
+import { shared_store } from './shared_store'
 
 export const Router = observer(() => {
 
     const path = get_url_location_path()
+
+    // If a client does not have token, redirect to login
+    if (!shared_store.is_auth()) window.location.hash = '#/login'
 
     return (isEqual(path, ['home']) || isEqual(path, [])) ? <HomePage />
         : isEqual(path, ['login']) ? < LoginPage />
