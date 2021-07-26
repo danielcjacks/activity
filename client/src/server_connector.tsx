dc6069ff318e1dc4558e6aac18668fbadddb3895
Josh Searle <joshryansearle@gmail.com>
2021-07-26 20:03:36 +1000

    Add headers from auth and revert error handling

--- client/src/server_connector.tsx
+++ client/src/server_connector.tsx
@@ -4,6 +4,8 @@ import { shared_store } from './shared_store'
  * @param server_route a server route like '/login'
  */
 export const server_post = async (server_route, body) => {
+  const jwt = shared_store.is_auth() ? shared_store.state.token : ''
+
   try {
     const response = await fetch(`http://localhost:5000${server_route}`, {
       method: 'POST',
@@ -12,18 +14,18 @@ export const server_post = async (server_route, body) => {
       redirect: 'follow',
       headers: {
         'Content-Type': 'application/json',
+        Authorization: `Bearer ${jwt}`,
       },
       referrerPolicy: 'no-referrer',
       body: JSON.stringify(body),
     })
 
     const json_response = await response.json()
-    json_response.error = !response.ok    
 
     if (json_response.error) {
       shared_store.show_toast(
         'error',
-        <pre>Error: {json_response?.message}</pre>
+        <pre>Error: {json_response.error.message}</pre>
       )
     }
     return json_response
