9948eb8fc39f39d30ec2e963af67402fc0f1e8bb
Josh Searle <joshryansearle@gmail.com>
2021-07-26 20:36:59 +1000

    Destructure login page elements

--- client/src/pages/login/login_page.tsx
+++ client/src/pages/login/login_page.tsx
@@ -9,53 +9,75 @@ import {
 import { observer } from 'mobx-react-lite'
 import { login_store } from './login_store'
 
-export const LoginPage = observer(() => {
+export const LoginPage = () => {
   return (
     <>
-      <Card>
-        <CardHeader title={'Login'} />
-      </Card>
+      <LoginTitle />
       <Box m={2}>
-        <Grid item xs={12} sm="auto">
-          <TextField
-            value={login_store.username}
-            onChange={(e: any) => (login_store.username = e.target.value)}
-            variant="filled"
-            label={login_store.is_error() ? 'Error' : 'Username'}
-            error={login_store.is_error()}
-            helperText={login_store.error_message}
-          />
-        </Grid>
-        <Grid item xs>
-          <TextField
-            type="password"
-            value={login_store.password}
-            onChange={(e: any) => (login_store.password = e.target.value)}
-            variant="filled"
-            label="Password"
-          />
-        </Grid>
-        <Grid item xs>
-          <Button
-            color="primary"
-            variant="contained"
-            onClick={login_store.login}
-            disabled={login_store.loading}
-          >
-            Login
-          </Button>
-        </Grid>
-        <Grid item xs>
-          <Button
-            color="secondary"
-            variant="contained"
-            onClick={login_store.signup}
-            disabled={login_store.loading}
-          >
-            Signup
-          </Button>
-        </Grid>
+        <LoginFields />
+        <LoginButtons />
       </Box>
+    </>
+  )
+}
+
+const LoginTitle = () => {
+  return (
+    <Card>
+      <CardHeader title={'Login'} />
+    </Card>
+  )
+}
+
+const LoginFields = observer(() => {
+  return (
+    <>
+      <Grid item xs={12} sm="auto">
+        <TextField
+          value={login_store.username}
+          onChange={(e: any) => (login_store.username = e.target.value)}
+          variant="filled"
+          label={login_store.is_error() ? 'Error' : 'Username'}
+          error={login_store.is_error()}
+          helperText={login_store.error_message}
+        />
+      </Grid>
+      <Grid item xs>
+        <TextField
+          type="password"
+          value={login_store.password}
+          onChange={(e: any) => (login_store.password = e.target.value)}
+          variant="filled"
+          label="Password"
+        />
+      </Grid>
+    </>
+  )
+})
+
+const LoginButtons = observer(() => {
+  return (
+    <>
+      <Grid item xs>
+        <Button
+          color="primary"
+          variant="contained"
+          onClick={login_store.login}
+          disabled={login_store.loading}
+        >
+          Login
+        </Button>
+      </Grid>
+      <Grid item xs>
+        <Button
+          color="secondary"
+          variant="contained"
+          onClick={login_store.signup}
+          disabled={login_store.loading}
+        >
+          Signup
+        </Button>
+      </Grid>
     </>
   )
 })
