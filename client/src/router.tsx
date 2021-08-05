ccc4b66d74b9055dc9dabe207df3fd6045fcfc09
Josh Searle <joshryansearle@gmail.com>
2021-08-05 20:55:32 +1000

    Add goal routes

--- client/src/router.tsx
+++ client/src/router.tsx
@@ -1,5 +1,7 @@
 import { isEqual } from 'lodash'
 import { observer } from 'mobx-react-lite'
+import { GoalsPage } from './pages/goals/goals_page'
+import { GoalPage } from './pages/goals/goal_page'
 import { HomePage } from './pages/home/home_page'
 import { LoginPage } from './pages/login/login_page'
 import { ValuesPage } from './pages/values/values_page'
@@ -18,6 +20,8 @@ export const Router = observer(() => {
         : isEqual(path, ['login']) ? < LoginPage />
         : isEqual(path, ['values']) ? < ValuesPage />
         : isEqual(path, ['values', 'create']) ? < ValuePage />
+        : isEqual(path, ['goals']) ? < GoalsPage />
+        : isEqual(path, ['goals', 'create']) ? <GoalPage />
         : <> {window.location.hash = '#/home'} Page {path.join('/')} Not found </>
 })
 
