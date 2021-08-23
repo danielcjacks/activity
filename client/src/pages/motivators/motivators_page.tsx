96812eaae18771b11ba9ecbd7a76e4b722fa789c
Josh Searle <joshryansearle@gmail.com>
2021-08-23 11:07:09 +1000

    Manual values page merge

--- client/src/pages/motivators/motivators_page.tsx
+++ client/src/pages/motivators/motivators_page.tsx
@@ -1,23 +1,67 @@
-import { Table, TableBody, TableCell, TableHead, TableRow } from '@material-ui/core'
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+  Card,
+  CardHeader,
+} from '@material-ui/core'
 import { observer } from 'mobx-react-lite'
-
+import { useState, useEffect } from 'react'
+import { server_post } from '../../server_connector'
+import { shared_store } from '../../shared_store'
 
 export const MotivatorsPage = observer(() => {
-    return <> Motivator table
+  return (
+    <>
+      <Card>
+        <CardHeader title={'Motivators'} />
+      </Card>
+      <MotivatorsTable />
     </>
+  )
 })
 
 const MotivatorsTable = observer(() => {
-    return <Table>
-        <TableHead>
-            <TableRow>
-                <TableCell>Name</TableCell>
-                <TableCell>Description</TableCell>
-                <TableCell>Importance</TableCell>
-            </TableRow>
-        </TableHead>
-        <TableBody>
-            
-        </TableBody>
+  const [motivators, set_motivators] = useState<any[]>([])
+
+  useEffect(() => {
+    //the `/prisma` route is hooked up on the backend.
+    //the `where: {userId: shared_store.state.userId}` means that only select the values of the current logged in user,
+    //similar to a `SELECT * WHERE` in SQL
+    server_post(`/prisma/motivator/findMany`, {
+      where: { user_id: shared_store.state.user_id },
+    })
+      .then((respond) => {
+        set_motivators(respond)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }, [])
+  return (
+    <Table>
+      <TableHead>
+        <TableRow>
+          <TableCell>Name</TableCell>
+          <TableCell>Description</TableCell>
+          <TableCell>Positivity</TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {motivators.map((motivators) => {
+          return (
+            <>
+              <TableRow>
+                <TableCell>{motivators.name}</TableCell>
+                <TableCell>{motivators.description}</TableCell>
+                <TableCell>{motivators.positivity}</TableCell>
+              </TableRow>
+            </>
+          )
+        })}
+      </TableBody>
     </Table>
+  )
 })
