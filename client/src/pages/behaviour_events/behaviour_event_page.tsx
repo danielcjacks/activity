57e9642feb80fb24efe0fba57f56b45774d15ac4
Josh Searle <joshryansearle@gmail.com>
2021-09-07 01:31:53 +1000

    Add event creation and deletion

--- client/src/pages/behaviour_events/behaviour_event_page.tsx
+++ client/src/pages/behaviour_events/behaviour_event_page.tsx
@@ -1,3 +1,80 @@
+import {
+  Card,
+  CardHeader,
+  Typography,
+  Grid,
+  TextField,
+  List,
+  ListItem,
+  FormControl,
+  ListItemIcon,
+  Box,
+  InputLabel,
+  Select,
+  IconButton,
+  MenuItem,
+} from '@material-ui/core'
+import AddIcon from '@material-ui/icons/Add'
+import RemoveIcon from '@material-ui/icons/Remove'
+import { action } from 'mobx'
+import { observer } from 'mobx-react-lite'
+import { useEffect } from 'react'
+import { router_store } from '../../router_store'
+import { event_store } from './behaviour_event_store'
+import { SaveButton } from '../../components/save_button'
+import { get_loading } from '../../utils/async_loaders'
+import { behaviour_store } from '../behaviours/behaviour_store'
+
 export const BehaviourEventPage = () => {
-  return null
+  useEffect(() => {
+    event_store.on_component_load()
+  }, [])
+  return (
+    <>
+      <Card>
+        <CardHeader title={`Behaviour Event`} />
+      </Card>
+      <Fields />
+      <SaveButton
+        can_save={true}
+        is_loading={get_loading(event_store, event_store.save)}
+        on_save={() => event_store.save()}
+      />
+    </>
+  )
 }
+
+const Fields = observer(() => {
+  return (
+    <>
+      <Grid item xs={12}>
+        <Typography>Behaviour</Typography>
+      </Grid>
+      <Grid item xs={12}>
+        <Select
+          style={{ width: '50%', margin: '1em', padding: '0.5em' }}
+          value={event_store.behaviour_id}
+          onChange={event_store.handle_select}
+        >
+          {event_store.behaviours.map((behaviour) => {
+            return <MenuItem value={behaviour.id}>{behaviour.name}</MenuItem>
+          })}
+        </Select>
+      </Grid>
+      <Grid item xs={12}>
+        <Grid item xs={12} sm="auto">
+          <TextField
+            value={event_store.comment}
+            onChange={action(
+              (e: any) => (event_store.comment = e.target.value)
+            )}
+            variant="filled"
+            multiline={true}
+            label="Comment"
+            fullWidth
+          />
+        </Grid>
+      </Grid>
+    </>
+  )
+})
---
+++ client/src/pages/behaviour_events/behaviour_event_store.ts
@@ -0,0 +1,115 @@
+import { makeAutoObservable, runInAction, toJS } from 'mobx'
+import { router_store } from '../../router_store'
+import { server_post } from '../../server_connector'
+import { shared_store } from '../../shared_store'
+import { setup_async_loaders } from '../../utils/async_loaders'
+
+class EventStore {
+  behaviour_id: number | null = null
+  behaviours: any[] = []
+  comment: string = ''
+
+  reset_state = () => {
+    this.behaviour_id = null
+    this.behaviours = []
+    this.comment = ''
+  }
+
+  is_update = () => {
+    const path = router_store.hash.split('/')
+    console.log(path)
+
+    return path[path.length - 1].split('?')[0] === 'update'
+  }
+
+  load_event = async () => {
+    const event_id = router_store.query.event_id
+    if (!event_id) return
+
+    const event = await server_post('/prisma/behaviourEvent/findUnique', {
+      where: {
+        id: +event_id,
+      },
+    })
+
+    runInAction(() => {
+      this.comment = event.comment
+      this.behaviour_id = event.behaviour_id
+    })
+  }
+
+  on_component_load = () => {
+    this.reset_state()
+    if (this.is_update()) {
+      // If in update mode, but no event_id, redirect home
+      if (!router_store.query.event_id) {
+        window.location.hash = '#/home'
+      }
+      this.load_event()
+    }
+    server_post('/prisma/behaviour/findMany', {
+      where: { user_id: shared_store.state.userId },
+    })
+      .then((response) => {
+        this.behaviours = response
+        this.behaviours.forEach((behaviour) => {
+          behaviour.id = +behaviour.id
+        })
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+
+  handle_select = (e: any) => {
+    this.behaviour_id = e.target.value
+  }
+
+  save = () => {
+    const is_update = this.is_update()
+    const event_id = router_store.query.event_id
+    const body = is_update
+      ? {
+          data: {
+            comment: this.comment,
+            behaviour_id: this.behaviour_id,
+          },
+          where: { id: +event_id },
+        }
+      : {
+          data: {
+            behaviour_id: this.behaviour_id,
+            comment: this.comment,
+          },
+        }
+
+    return server_post(
+      `/prisma/behaviourEvent/${is_update ? 'update' : 'create'}`,
+      body
+    )
+      .then((response) => {
+        if (response.error) {
+          shared_store.show_toast('error', response.error.message)
+          return
+        }
+        this.reset_state()
+        window.location.hash = '#/events'
+        shared_store.show_toast(
+          'success',
+          `Event ${is_update ? 'updated' : 'saved'}`
+        )
+      })
+      .catch((e) => {
+        shared_store.show_toast('error', 'Something went wrong')
+      })
+  }
+
+  update = () => {}
+
+  constructor() {
+    makeAutoObservable(this)
+    setup_async_loaders(this)
+  }
+}
+
+export const event_store = ((window as any).event_store = new EventStore())
