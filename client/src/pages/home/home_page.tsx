1fdbb8133801e64aca5147956c96ad4ab1187dce
Daniel <danielchaimjacks@gmail.com>
2021-09-09 17:11:11 -0400

    Add home page behaviour events

--- client/src/pages/home/home_page.tsx
+++ client/src/pages/home/home_page.tsx
@@ -1,6 +1,67 @@
+import { format } from 'date-fns'
 import { observer } from 'mobx-react-lite'
-
+import en_au from 'date-fns/locale/en-AU'
+import { cloneDeep, set } from 'lodash'
+import { Checkbox, FormControlLabel, Grid } from '@material-ui/core'
+import { home_store } from './home_store'
+import { useEffect } from 'react'
+import { action } from 'mobx'
 
 export const HomePage = observer(() => {
-    return <>Home page</>
+  useEffect(() => {
+    home_store.fetch_behaviours()
+  }, [])
+
+  return (
+    <>
+      <Greeting />
+      <BehaviourLog />
+    </>
+  )
+})
+
+const Greeting = observer(() => {
+  const date = new Date()
+  const locale = cloneDeep(en_au)
+
+  set(locale, ['localize', 'dayPeriod'], (day_period) => day_period)
+  // should be a string like 'morning' or 'evening'
+  const time_message = format(date, 'BBBB', { locale })
+
+  return <h1>Good {time_message}</h1>
+})
+
+const BehaviourLog = observer(() => {
+  return (
+    <div
+      style={{
+        display: 'grid',
+        gridTemplateColumns: 'repeat(auto-fill, minmax(200px, 1fr))',
+      }}
+    >
+      {home_store.behaviours.map((behaviour, i) => (
+        <BehaviourItem key={behaviour.id} behaviour_index={i} />
+      ))}
+    </div>
+  )
+})
+
+type Props = {
+  behaviour_index: number
+}
+
+const BehaviourItem = observer(({ behaviour_index }: Props) => {
+  const behaviour = home_store.behaviours[behaviour_index]
+
+  return (
+    <FormControlLabel
+      control={
+        <Checkbox
+          checked={!!behaviour.behaviour_event}
+          onChange={e => home_store.toggle_behaviour_event(behaviour_index)}
+        />
+      }
+      label={behaviour.name}
+    />
+  )
 })
--- client/src/pages/home/home_store.tsx
+++ client/src/pages/home/home_store.tsx
@@ -1,1 +1,64 @@
-export {}
+import cytoscape from 'cytoscape'
+import { sum } from 'lodash'
+import { makeAutoObservable, runInAction } from 'mobx'
+import { server_post } from '../../server_connector'
+import { shared_store } from '../../shared_store'
+import { setup_async_loaders } from '../../utils/async_loaders'
+
+class HomeStore {
+  behaviours: any[] = []
+
+  constructor() {
+    setup_async_loaders(this)
+    makeAutoObservable(this)
+  }
+
+  fetch_behaviours = async () => {
+    const behaviours = await server_post('/prisma/behaviour/findMany')
+    if (behaviours && !behaviours.error) {
+      runInAction(() => (this.behaviours = behaviours))
+    }
+  }
+
+  toggle_behaviour_event = async (behaviour_index: number) => {
+    const behaviour = this.behaviours[behaviour_index]
+    const is_created = !!behaviour.behaviour_event
+    if (is_created) {
+      await this.delete_behaviour_event(behaviour.behaviour_event.id)
+      delete behaviour.behaviour_event
+    } else {
+      const behaviour_event = await this.create_behaviour_event(behaviour.id)
+      if (behaviour_event) {
+        behaviour.behaviour_event = behaviour_event
+      }
+    }
+  }
+
+  create_behaviour_event = async (behaviour_id: number) => {
+    const behaviour_event = {
+      behaviour_id,
+    }
+    const response = await server_post('/prisma/behaviourEvent/create', {
+      data: behaviour_event,
+    })
+
+    if (response && !response.error) {
+      shared_store.show_toast('info', 'Added behaviour event')
+      return response
+    }
+  }
+
+  delete_behaviour_event = async (behaviour_event_id: number) => {
+    const response = await server_post('/prisma/behaviourEvent/delete', {
+      where: {
+        id: behaviour_event_id,
+      },
+    })
+
+    if (response && !response.error) {
+      shared_store.show_toast('info', 'Removed behaviour event')
+    }
+  }
+}
+
+export const home_store = ((window as any).home_store = new HomeStore())
