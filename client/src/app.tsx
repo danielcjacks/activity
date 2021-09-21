88d2d0c6ecd0e582ff375cb765f9f03360f7897f
Josh Searle <joshryansearle@gmail.com>
2021-09-21 18:21:35 +1000

    Create custom navbar to fix overflow

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -1,66 +1,148 @@
-import { Snackbar } from '@material-ui/core'
+import { IconButton, Snackbar } from '@material-ui/core'
 import { Alert } from '@material-ui/lab'
 import { observer } from 'mobx-react-lite'
 import { Router } from './router'
 import { shared_store } from './shared_store'
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import BottomNavigation from '@material-ui/core/BottomNavigation';
-import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
-import AssessmentIcon from '@material-ui/icons/Assessment';
-import HomeIcon from '@material-ui/icons/Home';
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import DirectionsRunIcon from '@material-ui/icons/DirectionsRun';
-import AddAlarmIcon from '@material-ui/icons/AddAlarm';
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import BottomNavigation from '@material-ui/core/BottomNavigation'
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction'
+import AssessmentIcon from '@material-ui/icons/Assessment'
+import HomeIcon from '@material-ui/icons/Home'
+import FavoriteIcon from '@material-ui/icons/Favorite'
+import DirectionsRunIcon from '@material-ui/icons/DirectionsRun'
+import AddAlarmIcon from '@material-ui/icons/AddAlarm'
 import { router_store } from './router_store'
+import { theme } from './theme'
 
 const useStyles = makeStyles({
-	root: {
-	  position: 'fixed',
-	  width: '100%',
-	  bottom: '0px',
-	},
-  });
+  root: {
+    position: 'fixed',
+    width: '100%',
+    bottom: '0px',
+  },
+})
 
 const Toasts = observer(() => {
-	return (
-		<Snackbar
-			anchorOrigin={{
-				vertical: 'bottom',
-				horizontal: 'center'
-			}}
-			open={shared_store.toast_is_open}
-			autoHideDuration={shared_store.toast_auto_hide_duration}
-			onClose={e => shared_store.set_toast_is_open(false)}
-		>
-			<Alert
-				variant='filled'
-				severity={shared_store.toast_severity}
-				onClose={e => shared_store.set_toast_is_open(false)}
-			>
-				{shared_store.toast_content}
-			</Alert>
-		</Snackbar>
-	)
+  return (
+    <Snackbar
+      anchorOrigin={{
+        vertical: 'bottom',
+        horizontal: 'center',
+      }}
+      open={shared_store.toast_is_open}
+      autoHideDuration={shared_store.toast_auto_hide_duration}
+      onClose={(e) => shared_store.set_toast_is_open(false)}
+    >
+      <Alert
+        variant="filled"
+        severity={shared_store.toast_severity}
+        onClose={(e) => shared_store.set_toast_is_open(false)}
+      >
+        {shared_store.toast_content}
+      </Alert>
+    </Snackbar>
+  )
 })
 
 export const App = observer(() => {
-	const classes = useStyles();
+  const current_path = router_store.hash.split('/').slice(0, 2).join('/')
 
-	const handleChange = (event: React.ChangeEvent<{}>, newValue: string) => {
-		window.location.hash = newValue;
-	};
+  const routes = [
+    {
+      label: 'Home',
+      link: '#/home',
+      icon: (
+        <HomeIcon color={'#/home' === current_path ? 'primary' : 'inherit'} />
+      ),
+    },
+    {
+      label: 'Motivators',
+      link: '#/motivators',
+      icon: (
+        <FavoriteIcon
+          color={'#/motivators' === current_path ? 'primary' : 'inherit'}
+        />
+      ),
+    },
+    {
+      label: 'Behaviours',
+      link: '#/behaviours',
+      icon: (
+        <DirectionsRunIcon
+          color={'#/behaviours' === current_path ? 'primary' : 'inherit'}
+        />
+      ),
+    },
+    {
+      label: 'Events',
+      link: '#/events',
+      icon: (
+        <AddAlarmIcon
+          color={'#/events' === current_path ? 'primary' : 'inherit'}
+        />
+      ),
+    },
+    {
+      label: 'Visualise',
+      link: '#/graph',
+      icon: (
+        <AssessmentIcon
+          color={'#/graph' === current_path ? 'primary' : 'inherit'}
+        />
+      ),
+    },
+  ]
 
-	
-	return <>
-    <Router />
-			<BottomNavigation value={router_store.hash} onChange={handleChange} className={classes.root} showLabels={false}>
-      	<BottomNavigationAction label="Home" value="#/home" icon={<HomeIcon />} />
-      	<BottomNavigationAction label="Motivators" value="#/motivators" icon={<FavoriteIcon />} />
-      	<BottomNavigationAction label="Behaviours" value="#/behaviours" icon={<DirectionsRunIcon />} />
-				<BottomNavigationAction label="Events" value="#/events" icon={<AddAlarmIcon />} />
-      	<BottomNavigationAction label="Visualise" value="#/graph" icon={<AssessmentIcon />} />
-    	</BottomNavigation>
+  const selected_index = routes.findIndex((route) => {
+    return route.link === current_path
+  })
+
+  return (
+    <>
+      <Router />
+      <div
+        style={{
+          position: 'fixed',
+          bottom: '0',
+          width: '100vw',
+          overflow: 'auto',
+          display: 'flex',
+          justifyContent: 'center',
+          backgroundColor: '#424242',
+        }}
+      >
+        {routes.map(({ label, link, icon }, i) => {
+          return (
+            <IconButton
+              style={{
+                maxWidth: '20vw',
+                width: '5em',
+                height: '2.5em',
+                borderRadius: '0',
+              }}
+              onClick={() => {
+                window.location.hash = link
+              }}
+            >
+              <div>
+                <div>{icon}</div>
+                {i === selected_index && (
+                  <div
+                    style={{
+                      fontSize: '0.8rem',
+                      color: theme.palette.primary.main,
+                    }}
+                  >
+                    {label}
+                  </div>
+                )}
+              </div>
+            </IconButton>
+          )
+        })}
+      </div>
       <Toasts />
     </>
+  )
 })
